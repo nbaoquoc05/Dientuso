8f3d0bd1c188852068c4c79a1dffc4ec698a3ae3
nbaoquoc05 <baoquoc2132005@gmail.com>
2025-07-02 20:39:34 +0700

    Update Bai13.vhd

--- Bai13.vhd
+++ Bai13.vhd
@@ -16,16 +16,13 @@ begin
         if clr = '1' then
             Q_int <= "0000";
         elsif rising_edge(clk) then
-            -- T0 luôn 0 -> giữ nguyên
-            -- T1 luôn toggle
+
             Q_int(1) <= not Q_int(1);
 
-            -- T2 toggle khi Q1 = 1
             if (Q_int(1) = '1') then
                 Q_int(2) <= not Q_int(2);
             end if;
 
-            -- T3 toggle khi Q2 and Q1 = 1
             if (Q_int(2) = '1' and Q_int(1) = '1') then
                 Q_int(3) <= not Q_int(3);
             end if;
